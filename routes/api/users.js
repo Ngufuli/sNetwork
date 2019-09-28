5c44e31d002986ad6902f2967831860b5e8d78dc
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-09-28 15:21:19 +0530

    Bcrypt authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,77 @@
 const express = require("express");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
+
+//load user models
+const User = require("../../models/User");
 
 //@route GET api/users/test
 //@desc  Test users route
-//@access
-router.get("/tests", (req, res) =>
+//@access Public
+router.get("/test", (req, res) =>
   res.json({
     msg: "Users works"
   })
 );
+
+//@route GET api/users/register
+//@desc  register user
+//@access Public
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", //Size
+        r: "pg", //Rating (Parental Guided)
+        d: "mm" //Default
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+//R@oute        GET api/users/login
+//@Description  Login/ Returning JWT Token
+//@access       Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //Finding the user by email
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+    //Checking the password------/remember the bcrypt encryption
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Successful" });
+      } else {
+        return res.status(400).json({ password: "Password incorrect" });
+      }
+    });
+  });
+});
 
 module.exports = router;
