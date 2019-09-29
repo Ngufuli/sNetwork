fefefd647db6f3caa5901fbda0e34adae3cd4599
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-09-29 18:40:59 +0530

    login validation

--- validation/login.js
+++ validation/login.js
@@ -1,7 +1,7 @@
 const Validator = require("validator");
 const isEmpty = require("./is-empty");
 
-module.exports = function validateRegisterInput(data) {
+module.exports = function validateLoginInput(data) {
   let errors = {};
 
   data.email = !isEmpty(data.email) ? data.email : "";
