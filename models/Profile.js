09e5d9527fa2adeea2e755e6903c1d2fef3d33bf
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-09-29 20:33:23 +0530

    Profile schema

--- models/Profile.js
+++ models/Profile.js
@@ -2,4 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 //Creating Schema
-const ProfileSchema = new Schema({});
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  }
+});
