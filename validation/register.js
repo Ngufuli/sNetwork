35409bffee0f8a81324b075f570b4ca20f091e05
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-09-29 16:07:52 +0530

    isEmpty function refactoring

--- validation/register.js
+++ validation/register.js
@@ -1,4 +1,5 @@
 const Validator = require("validator");
+import isEmpty from "./is-empty";
 
 module.exports = function validateRegisterInput(data) {
   let errors = {};
@@ -6,4 +7,8 @@ module.exports = function validateRegisterInput(data) {
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = "Name must be between 2 and 30 characters";
   }
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
 };
