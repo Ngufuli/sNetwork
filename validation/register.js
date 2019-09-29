5e04809f8ac59f45ce42c66598be4a61809e4e8e
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-09-29 15:30:59 +0530

    validator length for name

--- validation/register.js
+++ validation/register.js
@@ -2,4 +2,7 @@ const Validator = require("validator");
 
 module.exports = function validateRegisterInput(data) {
   let errors = {};
+
+  if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+  }
 };
