ed1f626124c28d780ddc1f937c83a3fb55b427e6
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-09-30 10:58:27 +0530

    Loaded the user model in profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,9 @@ const router = express.Router();
 //Load Profile model
 const Profile = require("../../models/Profile");
 
+//Load User model
+const User = require("../../models/User");
+
 //@route GET api/profile/test
 //@desc  Test profile route
 //@access
