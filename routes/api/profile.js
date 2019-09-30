bcbbc913e987a5f10af94508df350e8fa3f2e8ef
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-09-30 21:15:23 +0530

    Initialization of the social field

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -31,11 +31,19 @@ router.post(
     if (req.body.handle) profileFields.handle = req.body.handle;
     if (req.body.company) profileFields.company = req.body.company;
     if (req.body.website) profileFields.website = req.body.website;
-    if (req.body.handle) profileFields.handle = req.body.handle;
-    if (req.body.handle) profileFields.handle = req.body.handle;
-    if (req.body.handle) profileFields.handle = req.body.handle;
-    if (req.body.handle) profileFields.handle = req.body.handle;
-    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+
+    //Skills split into an array
+    if (typeof req.body.skills !== "undefined") {
+      profileFields.skills = req.body.skills.split(",");
+    }
+
+    //Social
+    profileFields.social = {};
     if (req.body.handle) profileFields.handle = req.body.handle;
     if (req.body.handle) profileFields.handle = req.body.handle;
     if (req.body.handle) profileFields.handle = req.body.handle;
