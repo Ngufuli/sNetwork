d68877f00eb7f80f3d4db238a7250852eb2d430d
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-09-30 21:02:05 +0530

    Profile fields

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -25,19 +25,7 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    const errors = {};
-
-    Profile.findOne({ user: req.user.id })
-      .then(profile => {
-        if (!profile) {
-          errors.noprofile = "There is no profile for this user";
-          return res.status(400).json(errors);
-        }
-        res.json(profile);
-      })
-      .catch(err => {
-        res.status(404).json(err);
-      });
+    const profileFields = {};
   }
 );
 
