c4a2142e6e69b02aedddda90899a47628be99673
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-01 19:34:27 +0530

    Handle validation is Profile

--- validation/profile.js
+++ validation/profile.js
@@ -8,6 +8,10 @@ module.exports = function validateProfileInput(data) {
   data.status = !isEmpty(data.status) ? data.status : "";
   data.skills = !isEmpty(data.skills) ? data.skills : "";
 
+  if (!Validator.isLength(data.handle, { min: 2, max: 40 })) {
+    errors.handle = "Handle needs to be between 2 and 4 characters";
+  }
+
   return {
     errors,
     isValid: isEmpty(errors)
