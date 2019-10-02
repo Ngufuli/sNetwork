0bcefd2857782e37ae24eca278c04c86ab8adb7f
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-02 13:35:50 +0530

    profile handle

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -39,11 +39,13 @@ router.get(
 router.get("/handle/:handle", (req, res) => {
   const errors = {};
   Profile.findOne({ handle: req.params.handle })
-    .populate("user", ["name", ["avatar"]])
+    .populate("user", ["name", "avatar"])
     .then(profile => {
       if (!profile) {
         errors.noprofile = "There is no profile for this user.";
+        res.status(404).json(errors);
       }
+      res.json(profile);
     });
 });
 
