4bb7c4733efd880e4b487c2c15759b3032a07831
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-02 15:52:08 +0530

    Experience route in Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -154,4 +154,21 @@ router.post(
   }
 );
 
+//@route POST api/profile/experience
+//@desc  Add experience to profile
+//@access Private
+router.get("/handle/:handle", (req, res) => {
+  const errors = {};
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user.";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 module.exports = router;
