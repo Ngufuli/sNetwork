400646f937e6d6d98ecd044495557814ba723f5c
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-02 15:53:56 +0530

    Adding passport authentication since it is private

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -157,18 +157,22 @@ router.post(
 //@route POST api/profile/experience
 //@desc  Add experience to profile
 //@access Private
-router.get("/handle/:handle", (req, res) => {
-  const errors = {};
-  Profile.findOne({ handle: req.params.handle })
-    .populate("user", ["name", "avatar"])
-    .then(profile => {
-      if (!profile) {
-        errors.noprofile = "There is no profile for this user.";
-        res.status(404).json(errors);
-      }
-      res.json(profile);
-    })
-    .catch(err => res.status(404).json(err));
-});
+router.post(
+  "/experience",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ handle: req.params.handle })
+      .populate("user", ["name", "avatar"])
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user.";
+          res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
