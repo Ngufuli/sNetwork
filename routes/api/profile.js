d672bfcc24370e355a9e8b902fe42f42006ad007
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-02 12:51:21 +0530

    fixed the get request for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -20,13 +20,16 @@ router.get(
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     const errors = {};
-    profile.findOne({ user: req.user.id }).then(profile => {
-      if (!profile) {
-        errors.noprofile = "There is no profile for this user";
-        return res.status(400).json(errors);
-      }
-      res.json(profile);
-    });
+    profile
+      .findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(400).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(400).json(err));
   }
 );
 
