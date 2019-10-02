fb94347e7df65f8af71662b590346714e5535164
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-02 13:47:02 +0530

    Error handling for user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -50,6 +50,25 @@ router.get("/handle/:handle", (req, res) => {
     .catch(err => res.status(404).json(err));
 });
 
+//@route GET api/profile/user/:user_id
+//@desc  Get profile by user ID
+//@access Public
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
+  Profile.findOne({ user: req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user.";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err =>
+      res.status(404).json({ profile: "There is no profile for this user" })
+    );
+});
+
 //@route POST api/profile/
 //@desc  Create or edit user profile
 //@access Private
