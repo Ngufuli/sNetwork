c13da12a6fdab9e3545ca6f3671d68d3ad780178
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-03 22:32:56 +0530

    Deletion of education for profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -244,4 +244,32 @@ router.delete(
   }
 );
 
+//@route DELETE api/profile/education/:exp_id
+//@desc  Delete education from profile
+//@access Private
+router.delete(
+  "/education/:edu_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        //Get remove experience
+        const removeIndex = profile.education
+          .map(item => item.id)
+          .indexOf(req.params.exp_id);
+
+        //Splice out of array
+        profile.education.splice(removeIndex, 1);
+
+        //Save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+//@route DELETE api/profile/
+//@desc  Delete user and profile
+//@access Private
+
 module.exports = router;
