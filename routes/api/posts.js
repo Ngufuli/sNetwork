addd0faf1b8cb7b7c4ab27a8522c99765aeb8c78
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-05 13:34:03 +0530

    Delete route for the post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,9 @@ const passport = require("passport");
 //Bringing up the post model
 const Post = require("../../models/Post");
 
+//Bringing up the profile model
+const Profile = require("../../models/Profile");
+
 //Validation
 const validatePostInput = require("../../validation/post");
 
@@ -55,6 +58,27 @@ router.post(
     });
 
     newPost.save().then(post => res.json(post));
+  }
+);
+
+//@route DELETE api/posts/:id
+//@desc  Delete posts
+//@access Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id).then(post => {
+        //Check for the owner of the post
+        if (post.user.toString() !== req.user.id) {
+          return res.status(401).json({ unauthorized: `User not authorized` });
+        }
+        //Delete
+        post.remove().then(() => res.json({ sucess: true }));
+      })
+      .catch(err=> res.status(404).json({postnotfound: `No post found`)})
+    });
   }
 );
 
