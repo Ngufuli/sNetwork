8c8c4545207fc5e75ba42abe1591c6523bb86002
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-05 14:39:22 +0530

    Added an unlike api in the post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -111,4 +111,38 @@ router.post(
     });
   }
 );
+
+//@route POST api/posts/unlike/:id
+//@desc  Unike posts
+//@access Private
+router.post(
+  "/unlike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length === 0
+          ) {
+            return res
+              .status(400)
+              .json({ notliked: `You have not yet liked this post` });
+          }
+          //Get the remove index
+          const removeIndex = post.likes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id);
+
+          //Splice out of the array
+          post.likes.splice(removeIndex, 1);
+          // Save
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: `No post found` }));
+      ``;
+    });
+  }
+);
 module.exports = router;
