1695836f72bfe86b9fe2f895b09774ede5cea06b
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-05 13:51:59 +0530

    Added the like api in the post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -85,4 +85,30 @@ router.delete(
   }
 );
 
+//@route POST api/posts/like/:id
+//@desc  Like posts
+//@access Private
+router.post(
+  "like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyliked: `User already liked this post` });
+          }
+          //Add user ID to the likes array
+          post.likes.unshift({ user: req.user.id });
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: `No post found` }));
+    });
+  }
+);
 module.exports = router;
