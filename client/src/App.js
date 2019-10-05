8cd99e942e10418f4f98ff8d9cc9f1c4039006ab
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-06 03:26:34 +0530

    Footer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>My React App</h1>
     </div>
   );
