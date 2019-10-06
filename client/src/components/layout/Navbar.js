24081ed96588424c514db63f6296932a7aea1367
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-06 11:02:49 +0530

    Finalized the navbar by changing a to link

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends Component {
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
-          <Link className="navbar-brand" to="/landing">
+          <Link className="navbar-brand" to="/">
             NQ|Dev
           </Link>
           <button
