4752863774b3c4b2cf6e44d69e6745a6b4bcde17
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-07 20:25:18 +0530

    Validation

--- validation/register.js
+++ validation/register.js
@@ -16,24 +16,27 @@ module.exports = function validateRegisterInput(data) {
   if (Validator.isEmpty(data.name)) {
     errors.name = "Name field is required";
   }
+
   if (Validator.isEmpty(data.email)) {
     errors.email = "Email field is required";
   }
+
   if (!Validator.isEmail(data.email)) {
     errors.email = "Email is invalid";
   }
+
   if (Validator.isEmpty(data.password)) {
     errors.password = "Password field is required";
   }
+
   if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = "Password must be atleast 6 characters";
+    errors.password = "Password must be at least 6 characters";
   }
+
   if (Validator.isEmpty(data.password2)) {
-    errors.password = "Confirm password field is required";
+    errors.password2 = "Confirm Password field is required";
   }
-  if (!Validator.isLength(data.password2, { min: 6, max: 30 })) {
-    errors.password2 = "Password must be atleast 6 characters";
-  }
+
   if (!Validator.equals(data.password, data.password2)) {
     errors.password2 = "Passwords must match";
   }
