8fbf5ee6155c6ceec2de8feae651668b36bacd22
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-08 14:11:21 +0530

    Connecting redux to the component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import classnames from "classnames";
+import { connect } from "react-redux";
 
 class Register extends Component {
   constructor() {
