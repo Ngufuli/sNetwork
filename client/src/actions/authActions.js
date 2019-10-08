1be97fe1ee4b27984fdfc30d45e31e1d4c3ab93f
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-08 22:44:42 +0530

    setting the authorization header

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -18,6 +18,18 @@ export const registerUser = (userData, history) => dispatch => {
 export const loginUser = userData => dispatch => {
   axios
     .post("/api/users/login", userData)
-    .then(res => {})
-    .catch(err => {});
+    .then(res => {
+      //Save to local storage
+      const { token } = res.data;
+      //Set token to local storage
+      localStorage.setItem("jwtToken", token);
+      //Set token to Auth header
+      setAuthToken(token);
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
 };
---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const setAuthToken = token => {
+  if (token) {
+    //Apply to every request
+    axios.defaults.header.common["Authorization"] = token;
+  } else {
+    //Delete auth header
+    delete axios.defaults.header.common["Authorization"];
+  }
+};
