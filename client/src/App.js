9dd54fbe3d7d706a08fd283bb8635bc563b9a0d5
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-10 21:01:06 +0530

    importing header for Persistant token after refress upon login

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
 import { Provider } from "react-redux";
 import store from "./store";
 
