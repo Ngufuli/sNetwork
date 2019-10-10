8364a72961b65bd6cae02fabdc07a86277dd94a9
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-10 19:54:50 +0530

    authorization token

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -3,10 +3,10 @@ import axios from "axios";
 const setAuthToken = token => {
   if (token) {
     //Apply to every request
-    axios.defaults.header.common["Authorization"] = token;
+    axios.defaults.headers.common["Authorization"] = token;
   } else {
     //Delete auth header
-    delete axios.defaults.header.common["Authorization"];
+    delete axios.defaults.headers.common["Authorization"];
   }
 };
 
