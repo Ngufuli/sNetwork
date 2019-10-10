bd3d8d29e872f37be0f6da606116239c205671d2
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-10 21:05:58 +0530

    Transition for App.js from stateless to statefull component

--- client/src/App.js
+++ client/src/App.js
@@ -8,28 +8,31 @@ import { Provider } from "react-redux";
 import store from "./store";
 
 import "./App.css";
+
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 
-function App() {
-  return (
-    <Provider store={store}>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+class App extends React.Component() {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
-    </Provider>
-  );
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
