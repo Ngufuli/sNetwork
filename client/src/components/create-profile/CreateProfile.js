958197ce6a589e888cdbae65664e2fce025927fa
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-20 17:07:24 +0530

    Added mapStateToProps and CreateProfile proptypes

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -3,9 +3,40 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
 class CreateProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displaySocialInputs: false,
+      handle: "",
+      company: "",
+      website: "",
+      location: "",
+      status: "",
+      skills: "",
+      githubusername: "",
+      bio: "",
+      twitter: "",
+      facebook: "",
+      linkedin: "",
+      youtube: "",
+      instagram: "",
+      errors: {}
+    };
+  }
+
   render() {
     return <div></div>;
   }
 }
+
+CreateProfile.propTypes = {
+  profile: PropTypes.object.isRequired,
+  errors: state.errors
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  errors: state.errors
+});
 
 export default connect()(CreateProfile);
