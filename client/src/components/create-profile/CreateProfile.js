42aeefebb42bf37bda697d47871e9bbb69cb9d1d
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-20 18:02:41 +0530

    TextAreaFieldGroup

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -33,7 +33,7 @@ class CreateProfile extends Component {
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Create Your Profile</h1>
               <p className="lead text-center">
-                Let's get some information to make y
+                Let's get some information to make your profile stand out
               </p>
               <small className="d-block pb-3">* = required fields</small>
             </div>
