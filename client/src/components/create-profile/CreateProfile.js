72836e6d339f7ba19289bf320e61ff08c8316fb5
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-20 16:58:21 +0530

    Create Profile component

---
+++ client/src/components/create-profile/CreateProfile.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class CreateProfile extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default connect()(CreateProfile);
