d25cb6e74f2338c603bf7394dfe0d44107813c99
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-22 10:52:59 +0530

    option key and values

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -42,6 +42,20 @@ class CreateProfile extends Component {
 
   render() {
     const { errors } = this.state;
+
+    //Select options for status
+    const options = [
+      { lable: "* Select Professional Status", value: 0 },
+      { label: "Developer", value: "Developer" },
+      { label: "Junior Developer", value: "Junior Developer" },
+      { label: "Senior Developer", value: "Senior Developer" },
+      { label: "Manager", value: "Mananger" },
+      { label: "Studend or Learning", value: "Student or Learning" },
+      { label: "Instructor or Teacher", value: "Instructor or Teacher" },
+      { label: "Intern", value: "Intern" },
+      { label: "Other", value: "Other" }
+    ];
+
     return (
       <div className="create-profile">
         <div className="container">
