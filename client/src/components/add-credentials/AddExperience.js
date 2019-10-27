2748c28e1ed5428eb1883f8e419bb60f8b1bfee4
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-27 20:34:31 +0530

    connect from react redux and proptypes

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { TextFieldGroup } from "../common/TextFieldGroup";
 import { TextAreaFieldGroup } from "../common/TextAreaFieldGroup";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 class AddExperience extends Component {
   render() {
