3f29c3dd6a470bc990ae8af8a87b4c14b8320204
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-28 03:49:28 +0530

    connect, proptypes and withrouter

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
 
 class Experience extends Component {
   render() {
