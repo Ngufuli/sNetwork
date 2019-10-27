45e0ddbe312a0ef479ceda2cebd319d1e3ce1f63
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-27 21:04:56 +0530

    Add Experience component

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -22,7 +22,24 @@ class AddExperience extends Component {
   }
   render() {
     const { errors } = this.state;
-    return <div className=""></div>;
+    return (
+      <div className="add-experience">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <Link to="/dashboard" className="btn btn-light">
+                Go Back
+              </Link>
+              <h1 className="display-4 text-center">Add Experience</h1>
+              <p className="lead text-center">
+                Add any job or position that you have had in the past or current
+              </p>
+              <small className="d-block pb-3">* = required field</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
