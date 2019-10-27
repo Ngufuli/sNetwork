372f060895c614631920bf835a828a8edbd4c8e9
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-27 20:40:58 +0530

    added form states in constructor

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -6,6 +6,20 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
 class AddExperience extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      company: "",
+      title: "",
+      location: "",
+      from: "",
+      to: "",
+      current: false,
+      description: "",
+      errors: {},
+      disabled: false
+    };
+  }
   render() {
     return <div></div>;
   }
