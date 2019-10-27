2be6f628f5747befeb1ef91baf5bf9d9eec5d734
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-27 20:28:30 +0530

    TextArea and TextFieldgroups

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
+import { TextFieldGroup } from "../common/TextFieldGroup";
+import { TextAreaFieldGroup } from "../common/TextAreaFieldGroup";
 
 class AddExperience extends Component {
   render() {
