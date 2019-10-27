176388e9dade94177d6f9ab9b8c01c27d05682be
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-28 03:57:10 +0530

    Table

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -5,6 +5,19 @@ import { withRouter } from "react-router-dom";
 
 class Experience extends Component {
   render() {
+    const experience = this.props.experience.map(exp => (
+      <tr key={exp._id}>
+        <td>{exp.company}</td>
+        <td>{exp.title}</td>
+        <td>
+          {exp.from} - {exp.to}
+        </td>
+        <td>
+          <button className="btn btn-danger">Delete</button>
+        </td>
+      </tr>
+    ));
+
     return <div></div>;
   }
 }
