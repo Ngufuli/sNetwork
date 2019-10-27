16749de61fe6467b7f48101dada802eda0e0bf29
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-27 20:24:37 +0530

    Add experience as class component, added link and withRouter

---
+++ client/src/components/add-credentials/AddExperience.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+
+class AddExperience extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default AddExperience;
