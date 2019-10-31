4634d46a0193c9e6fdc1f0883c0c8e57e91dab3d
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-31 22:20:42 +0530

    imported profile item

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import Spinner from "../common/Spinner";
+import ProfileItem from "./ProfileItem";
 import { getProfiles } from "../../actions/profileActions";
 
 class Profiles extends Component {
