f95bc1d37b18dde001ebf0f1a88c4c588c5bb0a4
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-31 22:06:45 +0530

    profile

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -6,7 +6,18 @@ import isEmpty from "../../validation/is-empty";
 class ProfileItem extends Component {
   render() {
     const { profile } = this.props;
-    return <div></div>;
+    return (
+      <div className="card card-body bg-light mb-3">
+        <div className="row">
+          <div className="col-2">
+            <img src={profile.user.avatar} alt="" className="rounded-circle" />
+          </div>
+          <div className="col-lg-6 col-md-4 col-8">
+            <h3>{profile.user.name}</h3>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
