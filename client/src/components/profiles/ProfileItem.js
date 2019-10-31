d826aa5f42a61462e61bb445fbd4b11ab00d492e
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-10-31 22:18:15 +0530

    font awesome check

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -29,6 +29,17 @@ class ProfileItem extends Component {
               View Profile
             </Link>
           </div>
+          <div className="col-md-4 d-none d-md-block">
+            <h4>Skill Set</h4>
+            <ul className="list-group">
+              {profile.skills.slice(0, 4).map((skill, index) => (
+                <li key={index} className="list-group-item">
+                  <i className="fa fa-check pr-1" />
+                  {skill}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     );
